4d75a1ee58e9361acd428b1c5454f38a85d7786c
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 16:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens.push(Ralph)
+  kittens.push("Ralph")
   return kittens
 }
