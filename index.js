71a0d60946c1c324225151dcd02f1bf33759efed
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 17:01:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function appendKitten(name){
 function prependKitten(name){
   return ["Arnold", ...kittens]
 }
-function removeLastKitten{}{
+function removeLastKitten{kittens}{
   kittens.slice(-1)
   return kitten
 }
