92013c6b30eb7d560b13571b8a0d1c4902e77cc8
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-11-06 16:48:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyPrependKitten(name){
   kittens.unshift("Bob")
   return kittens
 }
-function destructivelyRemoveLastKitten(kittens){
+function destructivelyRemoveLastKitten(name){
   kittens.pop()
   return kittens
 }
